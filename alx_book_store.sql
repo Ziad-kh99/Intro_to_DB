618db0399c4544f1436253829c7d2ab85fd11064
Ziad Khaled <ziadkh99@hotmail.com>
2025-02-01 00:38:36 +0200

    Convert some data types to upper-case in DDL statments

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,20 +25,20 @@ CREATE TABLE Customers (
 );
 
 CREATE TABLE Orders (
-    order_id int AUTO_INCREMENT PRIMARY KEY,
-    customer_id int,
-    order_date date,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 
 CREATE TABLE Order_Details (
-    order_detail_id int AUTO_INCREMENT PRIMARY KEY,
-    order_id int,
-    book_id int,
-    quantity double,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
 
