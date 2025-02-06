f8ba8dcd866cd82234f661a3f02786d4b90f9f0b
Ziad Khaled <ziadkh99@hotmail.com>
2025-02-06 21:25:32 +0200

    Modify table names to start with uppercase letter

--- task_2.sql
+++ task_2.sql
@@ -3,12 +3,12 @@
 USE alx_book_store
 
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 ); 
 
-CREATE TABLE books (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(130), 
     author_id INT, 
@@ -18,7 +18,7 @@ CREATE TABLE books (
     FOREIGN KEY (author_id) REFERENCES authors (author_id)
 );
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
@@ -26,7 +26,7 @@ CREATE TABLE customers (
 );
 
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -34,7 +34,7 @@ CREATE TABLE orders (
     FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
 );
 
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
